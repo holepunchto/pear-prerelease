817ff8e076b122f5e14f19d16259d9a2b4eb99b7
Mathias Buus <mathiasbuus@gmail.com>
2025-10-24 08:19:25 +0200

    add metadata and pull in prod first

--- index.js
+++ index.js
@@ -20,49 +20,80 @@ const drive = new Hyperdrive(store.namespace('release'), { compat: false })
 await drive.ready()
 
 const prod = new Hyperdrive(store.namespace('prod'), PROD_KEY)
-
-console.log(drive.blobs.core.length, drive.blobs.core.byteLength, id.encode(await drive.blobs.core.treeHash()))
-console.log(drive.core.length, id.encode(await drive.core.treeHash()))
+await prod.ready()
 
 const stage = new Hyperdrive(store.session(), STAGE_KEY)
 await stage.ready()
-
-const co = stage.checkout(STAGE_CHECKOUT)
-await co.ready()
 
 swarm.on('connection', c => store.replicate(c))
 swarm.join(drive.discoveryKey, {
   client: true,
   server: true
 })
-swarm.join(co.discoveryKey, {
+swarm.join(stage.discoveryKey, {
   client: true,
   server: false
 })
 
+// hydrate prod target
+if (prod.core.length === 0) await new Promise(resolve => prod.core.once('append', () => resolve()))
+
+prod.core.download()
+
+console.log('Copying in existing metadata data, might take a bit...')
+while (drive.core.length < prod.core.length) {
+  await drive.core.append(await prod.core.get(drive.core.length))
+  console.log('Copied blocks', drive.core.length, '/', prod.core.length)
+}
+console.log('Done!')
+console.log()
+
+await drive.getBlobs()
+await prod.getBlobs()
+
+prod.blobs.core.download()
+
+console.log('Copying in existing blob data, might take a bit...')
+while (drive.blobs.core.length < prod.blobs.core.length) {
+  await drive.blobs.core.append(await prod.blobs.core.get(drive.blobs.core.length))
+  console.log('Copied blob blocks', drive.blobs.core.length, '/', prod.blobs.core.length)
+}
+console.log('Done!')
+console.log()
+
+const co = stage.checkout(STAGE_CHECKOUT)
+await co.ready()
+
 let n = 0
 
+console.log('Checking diff')
 for await (const data of co.mirror(drive, { dryRun: true, batch: true })) print(data)
+if (!n) console.log('(Empty)')
+console.log('Done!')
+console.log()
 
 const pkg = JSON.parse(await co.get('/package.json'))
-console.log()
-console.log('total changes', n)
-console.log('version:', pkg.version)
+console.log('Total changes', n)
+console.log('Package version:', pkg.version)
 console.log()
 
+console.log('Core:')
 console.log(drive.core.id, drive.core.length)
 console.log(id.encode(await drive.core.treeHash()))
-console.log(drive.blobs.core.length)
+console.log()
+console.log('Blobs:')
+console.log(drive.blobs.core.id, drive.blobs.core.length)
 console.log(id.encode(await drive.blobs.core.treeHash()))
+console.log()
 
 if (DRY_RUN) {
-  console.log('exiting due to dry run...')
+  console.log('Exiting due to dry run...')
   await swarm.destroy()
   process.exit(0)
 }
 
 if (!/^\d+\.\d+\.\d+$/.test(pkg.version)) {
-  console.log('version does not look like a release version to for dry-running...')
+  console.log('Version does not look like a release version to for dry-running...')
   await swarm.destroy()
   process.exit(0)
 }
@@ -73,9 +104,33 @@ console.log('OK THEN! Staging...')
 
 console.log()
 for await (const data of co.mirror(drive, { batch: true })) print(data)
+if (!n) console.log('(Empty)')
 console.log()
 
-console.log('DONE!')
+// skipping release as thats non sensical
+const keys = ['metadata', 'channel', 'platformVersion', 'warmup']
+
+for (const k of keys) {
+  const from = await co.db.get(k)
+  const to = await stage.db.get(k)
+
+  if (!from && !to) {
+    continue
+  }
+
+  if (!from && to) {
+    console.log('Dropping pear setting', key)
+    await stage.db.del(k)
+    continue
+  }
+
+  if ((from && !to) || (JSON.stringify(from.value) !== JSON.stringify(to.value))) {
+    console.log('Updating pear setting', key)
+    await stage.db.put(key, from.value)
+  }
+}
+
+console.log('Done!')
 console.log(drive.core)
 console.log()
 console.log('Swarming until you exit...')
